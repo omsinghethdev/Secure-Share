247a011e0471619cc4eb6ad8cf57d08565f3d5e9
Om kumar singh <omsingh.ethdev@gmail.com>
2026-05-18 22:51:36 +0530

    feat: add SSH key generation setup

--- bin/shellbridge.sh
+++ bin/shellbridge.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 #current version tested on macOS en0 interface
+# 1. Detect local Network
 ip_addr=$(ifconfig en0 | grep "inet" | grep "netmask" | awk '{print $2}')
 networkPrefix=$(echo "${ip_addr}" | awk -F "." '{print $1"."$2"."$3}')
-
+# 2. Scan Network
 ping_log="/Users/omkumarsingh/Desktop/LinuxProjects/ShellBridge/temp/ping-result.txt"
 
 > "${ping_log}"
@@ -15,12 +16,14 @@ while [[ ${count} -le 254 ]]; do
 ) &  
        (( count=count+1 ))
 done
+wait
+# 3. Filter Acitve Devices
 active_devices="/Users/omkumarsingh/Desktop/LinuxProjects/ShellBridge/temp/active.txt"
 > "$active_devices"
 cat "${ping_log}" | grep -F '64 bytes from' >> "${active_devices}"
 
 active_device_ip=($(cat "${active_devices}" | awk '{print $4 }'| sed 's/:$//') )
-
+# 4. Display and Select Device
 index=1
 
 for ip in "${active_device_ip[@]}"; do
@@ -32,6 +35,17 @@ read -p "Select device number:" choice
 
 selected_ip="${active_device_ip[$((choice - 1))]}"
 echo "Selected device: ${selected_ip}"
+read -p "Enter remote user name:" remote_user
+# 5. SSH key Setup
+ssh_private_key="/Users/omkumarsingh/.ssh/id_ed25519"
+ssh_pub_key="/Users/omkumarsingh/.ssh/id_ed25.pub519"
+if [[ -f  "${ssh_private_key}" && -f "${ssh_pub_key}" ]]; then
+    echo "Ssh Key pair already exits."
+else 
+    echo "Generating keys..."
+    ssh-keygen -t ed25519 -f "${ssh_private_key}"
+fi
+    
 
 
 
